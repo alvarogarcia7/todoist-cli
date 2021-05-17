0f5029c511232b2eb554f3ac90362d5aa93b1f80
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-05-17 08:22:50 +0400

    Remove cache from downloader

--- functions.sh
+++ functions.sh
@@ -6,11 +6,7 @@ function download_GET(){
   local file="$1"
   local url="$2"
 
-  if [ -e "$file" ]; then
-    echo "File $file exists"
-  else
-    curl -X GET \
-      "$url" \
-      -H "Authorization: Bearer $TODOIST_API_TOKEN" > "$file"
-  fi
+  curl -X GET \
+    "$url" \
+    -H "Authorization: Bearer $TODOIST_API_TOKEN" > "$file"
 }
